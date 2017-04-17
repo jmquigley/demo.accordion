d2b837cbebdc62e98ce0633b5d91dd8053d77e92
James Quigley <jmquigley@outlook.com>
2017-04-17 14:53:45 -0500

    Adds redux to the demo app
    Shows how to connect redux to the app to manage the state of the
    component.

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ This will create an `h1` and a `p`aragraph.  When the h1 is clicked, the paragra
 
 The point of this sample is to show how to properly create a ReactJS class using Typescript.  It also shows how *properties* and *state* can be used within a React class control.  Last of all it demonstrates the binding of an event listener function ([this](http://reactkungfu.com/2015/07/why-and-how-to-bind-methods-in-your-react-component-classes/) and [this](http://stackoverflow.com/questions/32317154/uncaught-typeerror-cannot-read-property-setstate-of-undefined) explain why this is needed; this is an ugly javascript gotcha).
 
+The demo also shows how to use redux to manage the state of the application.
+
 
 ## Usage
 
--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {connect, Provider} from 'react-redux';
 
-interface AccordionState {
-	active: boolean;
+interface ActionFn {
+	(): any;
 }
 
-interface AccordionProps {
+interface State {
+	active?: boolean;
+}
+
+interface Dispatch {
+	toggle?: ActionFn;
+}
+
+interface AccordionProps extends State, Dispatch {
 	header?: string;
 	message?: string;
+}
+
+interface Action {
+	type: string;
+	val?: any;
 }
 
 let styles = {
@@ -19,37 +34,72 @@ let styles = {
 	}
 }
 
-export class Accordion extends React.Component<AccordionProps, AccordionState> {
+let defaultProps: AccordionProps = {
+	header: "Hello from React",
+	message: "Lorem Ipsum"
+};
 
-	static defaultProps = {
-		header: "Hello from React",
-		message: "Lorem Ipsum"
+
+/* Reducer function for toggling current state */
+export let toggleAccordion = (state: State, action: Action): State => {
+	switch(action.type) {
+		case '@@redux/INIT':
+			console.log('Init toggle');
+			return {
+				...state,
+				active: true
+			}
+
+		case 'TOGGLE':
+			console.log(`toggle: ${state.active}`);
+			return {
+				...state,
+				active: !state.active
+			}
 	}
+
+	return state;
+}
+
+const mapStateToProps = (state: State, ownProps: AccordionProps): AccordionProps => {
+	// The ownProps represent the properties passed to Accordion
+	// This will assign default properties when ownProps are null
+	let props = Object.assign(defaultProps, ownProps);
+
+	return {
+		...props,
+		active: state.active
+	}
+}
+
+const mapDispatchToProps = (dispatch: any): Dispatch => {
+	return {
+		toggle: () => {
+			dispatch({type: 'TOGGLE'})
+		}
+	}
+}
+
+@connect<State, Dispatch, any>(mapStateToProps, mapDispatchToProps)
+class Accordion extends React.Component<AccordionProps, any> {
 
 	constructor(props: AccordionProps) {
 		super(props);
-
-		this.state = {
-			active: true
-		};
-
-		this.toggle = this.toggle.bind(this);
-	}
-
-	toggle() {
-		this.setState({
-			active: !this.state.active
-		});
 	}
 
 	render() {
 		return (
 			<div>
-				<h1 onClick={this.toggle}>{this.props.header}</h1>
-				<p style={this.state.active ? styles.active : styles.inactive}>{this.props.message}</p>
+				<h1 onClick={this.props.toggle}>{this.props.header}</h1>
+				<p style={this.props.active ? styles.active : styles.inactive}>{this.props.message}</p>
 			</div>
 		);
 	}
 }
 
-ReactDOM.render(<Accordion header="blah, blah, blah" />, document.getElementById('app'));
+ReactDOM.render(
+	<Provider store={createStore(toggleAccordion)}>
+		<Accordion header="blah, blah, blah" />
+	</Provider>,
+	document.getElementById('app')
+);
